c97678db6bcf1f2cb76293c9fafb466bff22da74
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-06 16:06:23 -0700

    Created feed and styled sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,20 @@ import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">
+                #
+            </span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <img src="" alt=""/>
+                <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3J" alt=""/>
                 <Avatar className="sidebar__avatar" />
                 <h2>Beknaz Baktygulov</h2>
                 <h4>enelbbs9@gmail.com</h4>
@@ -30,7 +40,11 @@ function Sidebar() {
 
             <div className="sidebar__bottom">
                 <p>Recent</p>
-                
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
             </div>
         </div>
     )

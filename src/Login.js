9b2829aa3d7bcfd1551c8244f533fa99bc299acd
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-10 12:16:52 -0700

    Developed target value reader for each piece of state

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 function Login() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ profilePic, setProfilePic ] = useState('');
 
 const loginToApp = () => {
-    
+  
+
 }
 
 const register = () => {
-
+    
 }
 
     return (
@@ -16,14 +21,14 @@ const register = () => {
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png" alt=""/>
         
         <form>
-            <input placeholder="Full name (required if registering)" type="text"/>
-            <input placeholder="Profile pic URL (optional)" type="text"/>
-            <input placeholder="Email" type="email"/>
-            <input placeholder="Password" type="password"/>
+            <input value={name} onChange={e => setName(e.target.value)} placeholder="Full name (required if registering)" type="text"/>
+            <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder="Profile pic URL (optional)" type="text"/>
+            <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email"/>
+            <input  value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password"/>
             <button type="submit" onClick={loginToApp}>Sign In</button>
         </form>
         <p>Not a member?
-            <span className="login__register" onClick={register}>Register Now</span>
+            <span className="login__register" onClick={register}>     Register Now</span>
         </p>
         </div>
     )

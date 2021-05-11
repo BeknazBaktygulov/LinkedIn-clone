1eb55435b4926d03c77ded5fa66dc6f46c4b531c
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-11 14:59:11 -0700

    Brought selectUser slice from redux store to sidebar component to render that data on screen

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/counter/userSlice';
 import './Sidebar.css';
 
 function Sidebar() {
-
+    const user = useSelector(selectUser)
     const recentItem = (topic) => (
         <div className="sidebar__recentItem">
             <span className="sidebar__hash">
@@ -17,9 +19,9 @@ function Sidebar() {
         <div className="sidebar">
             <div className="sidebar__top">
                 <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3J" alt=""/>
-                <Avatar className="sidebar__avatar" />
-                <h2>Beknaz Baktygulov</h2>
-                <h4>enelbbs9@gmail.com</h4>
+                <Avatar sric={user.photoUrl} className="sidebar__avatar" />
+                <h2>{user.displayName}</h2>
+                <h4>{user.email}</h4>
             </div>
             <div className="sidebar__stats">
                 <div className="sidebar__stat">
